2f5954a47918929f6f65a827cecfdf200dc43366
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2023-02-02 22:44:59 -0500

    change header

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,9 @@
 export default function Index() {
   return (
     <div>
-      <h1 className="text-6xl font-bold text-blue-400">Welcome to Remix</h1>
+      <h1 className="text-6xl font-bold text-blue-400">
+        Welcome to Cheese And Paws
+      </h1>
     </div>
   );
 }
